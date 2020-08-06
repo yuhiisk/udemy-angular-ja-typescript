b52c1ba115fa977face1a77cef9c4b1557e96d8f
yuhiisk <yisokawa@popcorns.jp>
2020-08-06 09:20:21 +0900

    1 file modified

--- 01-types.ts
+++ 01-types.ts
@@ -6,56 +6,20 @@ title = 'Angular!';
 var n: number = 9;
 
 // 真偽値型
-var isOpen: boolean = true; // or false
+var isOpen: boolean = true;
 
 // 配列型
-var keywords: string[] = ['name', 'email', 'body'];
+var keywords: (string|number)[] = ['name', 'email', 'body', 0];
 
-// タプル型
-var payment: [number, string, number] = [1, 'apple', 3000];
-var _payment: (string|number)[] = [1, 'apple', 3000];
+// タブル型
+var payment: [number, string, number] = [1, 'apple', 0];
 
 // オブジェクト型
-var options: { [key: string]: string } = {
+var object: { [key: string]: string } = {
   name: 'Yohei Isokawa',
-  test: 'this is test'
+  id: 'test'
 };
-// array or object. (typeof演算子でobject判定できるデータ)
 var post: object = [{ id: 12, content: 'lorem ipsum' }];
-
-// enum型
-enum KeyCode {
-  Backspace = 8,
-  Tab = 9,
-  Enter = 13,
-  Shift = 16,
-  Ctrl = 17,
-  Alt = 18,
-  PauseBreak = 19,
-}
-
-console.log(KeyCode.Backspace);
-
-enum ErrorCode {
-  OK = 200,
-  BAD_REQUEST = 400,
-  UNAUTHORIZED = 401,
-}
-
-console.log(ErrorCode.OK, ErrorCode.BAD_REQUEST);
-
-enum Character {
-  Bob,
-  Dad,
-  Mom,
-}
-
-console.log(Character.Bob, Character.Dad, Character.Mom);
-
-var something: any = 'foo';
-something = 100;
-something = true;
-
 
 // 共用型(Union型)
 var sample: string | number | boolean;
@@ -63,10 +27,40 @@ sample = 'Yohei';
 sample = 100;
 sample = true;
 
-// null, undefined
+// enum型
+enum KeyCode {
+  Backspace = 8,
+  Tab = 9,
+  Enter = 13,
+}
+
+console.log(KeyCode.Backspace); // = 8
+
+enum ErrorCode {
+  OK = 200,
+  BAD_RESPONSE = 400,
+  UNAUTHORIZED = 401,
+}
+
+console.log(ErrorCode.OK);
+
+enum Character {
+  Bob, // 0
+  Dad, // 1
+  Mom, // 2
+}
+
+console.log(Character.Bob);
+
+// any型
+var something: any = 'foo';
+something = 100;
+something = true;
+something = { id: 1 };
+
+// null, undefined型
 var el: Element | null = document.querySelector('#app');
 var foo: undefined = undefined;
-// console.log(el);
 
 // 型推論
 var unknown = 'string';
