ef6f9766ada1c756f81d72db205c95eb350001d5
yuhiisk <yisokawa@popcorns.jp>
2020-08-10 09:04:15 +0900

    1 file added

---
+++ 12-interface.ts
@@ -0,0 +1,33 @@
+// インターフェイス
+interface Animal {
+  name: string;
+  legs: number;
+  isCry: boolean;
+  cry(): void;
+}
+
+interface SuperAnimal extends Animal {
+  canRun: boolean;
+}
+
+class Dog implements SuperAnimal {
+  name: string = 'Maru';
+  legs: number = 4;
+  isCry: boolean = true;
+  canRun: boolean = true;
+  cry(): void {
+    if (this.isCry) {
+      console.log('Bow, wow!');
+    } 
+  }
+}
+
+// オブジェクトにも適用可能
+// let Dog: Animal = {
+//   name: 'Maru',
+//   legs: 4,
+//   isCry: true,
+//   cry: () => {
+//     console.log('Bow, wow!');
+//   }
+// };
