89200483ef6b967957c5cdbd379e624090d71ef6
yuhiisk <yisokawa@popcorns.jp>
2020-08-22 06:14:38 +0900

    1 file modified

--- 06-function.ts
+++ 06-function.ts
@@ -1,14 +1,13 @@
-// 関数 (Function)
+// 関数
 function greet(name: string = 'Yohei'): void {
-  console.log(`Hello World! ${name}!!`);
+  console.log(`Hello world! ${name}`);
 }
-greet();
-greet('Bob!');
+greet('Taro');
 
 // 可変長引数
 function sum(...values: number[]): number {
-  return values.reduce((prev, current) => {
+  return values.reduce(function(prev, current) {
     return prev + current;
   });
 }
-console.log(sum(1, 3, 5, 8));
+console.log(sum(1, 3, 5, 9));
